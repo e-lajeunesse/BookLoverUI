f4c698e21a4f3f6c2b653620c149523281a52ad4
e-lajeunesse <eric.lajeunesse@outlook.com>
2021-03-17 13:21:14 -0400

    Add Book methods

--- BookLoverUI/BookMenu.cs
+++ BookLoverUI/BookMenu.cs
@@ -15,7 +15,7 @@ namespace BookLoverUI
             Console.Clear();
             Console.WriteLine("What would you like to do?\n" +
                 "1.Browse all books\n" +
-                "2.Find a book by name\n" +
+                "2.Find a book by title\n" +
                 "3.Add a new book\n" +
                 "0.Go back to main menu");
 
@@ -25,12 +25,11 @@ namespace BookLoverUI
                 case "1":
                     DisplayAllBooks();
                     break;
-                case "2":
-                    //add method to look up book by name
+                case "2":                    
                     GetBookByTitle();
                     break;
                 case "3":
-                    // add method to add a book
+                    AddBookMenu();                    
                     break;
                 case "0":
                     break;
@@ -100,6 +99,76 @@ namespace BookLoverUI
             {
                 Console.WriteLine("\nBook not found");
             }
+            Console.ReadKey();
+        }
+
+        public void AddBookMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("How would you like to add the book(s)?\n" +
+                "1.Enter all information manually\n" +
+                "2.Add book by title and author name using Google Books API\n" +
+                "3.Add all books by an author using Google Books API\n" +
+                "0.Go back to previous menu");
+            string userSelection = Console.ReadLine();
+
+            switch (userSelection)
+            {
+                case "1":                    
+                    AddBookManually();
+                    break;
+                case "2":                    
+                    AddBookWithTitleAndName();
+                    break;
+                case "3":
+                    AddBooksByAuthor();                    
+                    break;
+                case "0":
+                    break;
+                default:
+                    Console.WriteLine("Invalid entry");
+                    AddBookMenu();
+                    break;
+            }
+        }
+
+        public void AddBookManually()
+        {
+            Console.Clear();
+            Console.Write("Enter book title: ");
+            string title = Console.ReadLine();
+            Console.Write("\nEnter Genre: ");
+            string genre = Console.ReadLine();
+            Console.Write("\nEnter Book Description: ");
+            string description = Console.ReadLine();
+            Console.Write("Enter Author Id Number: ");
+            int authorId = int.Parse(Console.ReadLine());
+            string wasAdded = BookLoverUI.Service.AddBook(title, genre, description, authorId).Result;
+            Console.WriteLine(wasAdded);
+            Console.ReadKey();
+        }
+
+        public void AddBookWithTitleAndName()
+        {
+            Console.Clear();
+            Console.Write("Enter Book Title: ");
+            string title = Console.ReadLine();
+            Console.Write("\nEnter Author's Full Name: ");
+            string name = Console.ReadLine();
+            string wasAdded = BookLoverUI.Service.AddBookByTitleAndName(title, name).Result;
+            Console.WriteLine(wasAdded);
+            Console.ReadKey();
+        }
+
+        public void AddBooksByAuthor()
+        {
+            Console.Clear();
+            Console.Write("Enter Author First Name: ");
+            string firstName = Console.ReadLine();
+            Console.Write("\nEnter Author Last Name: ");
+            string lastName = Console.ReadLine();
+            string wasAdded = BookLoverUI.Service.AddBooksByAuthor(firstName, lastName).Result;
+            Console.WriteLine(wasAdded);
             Console.ReadKey();
         }
     }

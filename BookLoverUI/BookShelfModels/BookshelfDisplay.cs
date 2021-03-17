d18f1a0c36f6229d73a47ebef6563cedb9450c56
e-lajeunesse <eric.lajeunesse@outlook.com>
2021-03-16 21:26:13 -0400

    Get Book By Title

---
+++ BookLoverUI/BookShelfModels/BookshelfDisplay.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookLoverUI.BookShelfModels
+{
+    public class BookshelfDisplay
+    {
+        public int BookshelfId { get; set; }
+        public string Title { get; set; }
+        public List<BookshelfBookDisplay> Books { get; set; }
+    }
+}

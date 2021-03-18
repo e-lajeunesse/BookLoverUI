27c91dbbbbd8efd6735fe55583b306953e6ce942
Marley Boone <mboone30@ivyhtech.edu>
2021-03-18 09:46:21 -0400

    Working Delete endpoint for UI

--- BookLoverUI/BookLoverService.cs
+++ BookLoverUI/BookLoverService.cs
@@ -3,7 +3,7 @@
 using BookLoverUI.BookModels;
 using BookLoverUI.BookShelfModels;
 
-﻿using BookLoverUI.BookModels;
+//﻿using BookLoverUI.BookModels;
 using BookLoverUI.BookReviewModels;
 
 using System;
@@ -220,7 +220,7 @@ namespace BookLoverUI
             return null;
         }
 
-        public async Task<BookReviewEdit> UpdateBookReview()
+        /*public async Task<BookReviewEdit> UpdateBookReview()
         {
             _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", $"{AccessToken}");
             HttpResponseMessage response = _client.GetAsync($"https://localhost:44388/api/BookReview").Result;
@@ -231,20 +231,21 @@ namespace BookLoverUI
                 return bookReview;
             }
             return null;
-        }
-
-       /* public async Task<BookReviewDisplayItem> DeleteBookReviewById(int id)
-        {
-            _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", $"{AccessToken}");
-            HttpResponseMessage response = _client.GetAsync($"https://localhost:44388/api/BookReview/{id}").Result;
-           
-            if (response.IsSuccessStatusCode)
-            {
-                BookReviewDisplayItem bookReview = await response.Content.ReadAsAsync<BookReviewDisplayItem>();
-                return bookReview;
-            }
-            return null;
         }*/
+
+
+        public async Task<BookReviewDisplayItem> DeleteBookReviewById(int id)
+         {
+             _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", $"{AccessToken}");
+             HttpResponseMessage response = _client.DeleteAsync($"https://localhost:44388/api/BookReview/{id}").Result;
+
+             if (response.IsSuccessStatusCode)
+             {
+                 BookReviewDisplayItem bookReview = await response.Content.ReadAsAsync<BookReviewDisplayItem>();
+                 return bookReview;
+             }
+             return null;
+         }
 
         // Bookshelf Methods
         public async Task<string> CreateBookshelf(string title, List<int> bookIds)
